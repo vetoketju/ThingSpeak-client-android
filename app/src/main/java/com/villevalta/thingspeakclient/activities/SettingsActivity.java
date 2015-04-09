1e9d2ca75cc57e6dad5730814d4b32dc453f64bd
Ville Valta <ville.valta@gmail.com>
2015-04-09 17:15:33 +0300

    added settingsActivity

---
+++ app/src/main/java/com/villevalta/thingspeakclient/activities/SettingsActivity.java
@@ -0,0 +1,9 @@
+package com.villevalta.thingspeakclient.activities;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by villevalta on 9.4.2015.
+ */
+public class SettingsActivity extends ActionBarActivity {
+}
