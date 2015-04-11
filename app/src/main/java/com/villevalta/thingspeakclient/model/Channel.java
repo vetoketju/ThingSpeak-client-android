45b8808b3e1711782e1f4ef9b7a4e057dfe2e080
vetoketju <ville.valta@gmail.com>
2015-04-11 11:01:09 +0300

    Added getField

--- app/src/main/java/com/villevalta/thingspeakclient/model/Channel.java
+++ app/src/main/java/com/villevalta/thingspeakclient/model/Channel.java
@@ -224,4 +224,18 @@ public class Channel implements ListViewable{
 	public int getViewType() {
 		return R.layout.listitem_channel;
 	}
+
+	public String getField(int field) {
+		switch (field){
+			case 1: return field1;
+			case 2: return field2;
+			case 3: return field3;
+			case 4: return field4;
+			case 5: return field5;
+			case 6: return field6;
+			case 7: return field7;
+			case 8: return field8;
+			default: return null;
+		}
+	}
 }
