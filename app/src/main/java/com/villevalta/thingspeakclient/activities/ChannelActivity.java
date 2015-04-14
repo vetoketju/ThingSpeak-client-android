672f899166a3a0dd6d2ab6bc2990f24955eb4642
Ville Valta <ville.valta@gmail.com>
2015-04-14 17:48:06 +0300

    Added error and retry logic

--- app/src/main/java/com/villevalta/thingspeakclient/activities/ChannelActivity.java
+++ app/src/main/java/com/villevalta/thingspeakclient/activities/ChannelActivity.java
@@ -3,12 +3,14 @@ package com.villevalta.thingspeakclient.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.mikephil.charting.charts.LineChart;
+import com.beardedhen.androidbootstrap.BootstrapButton;
 import com.github.mikephil.charting.data.LineData;
 import com.villevalta.thingspeakclient.R;
 import com.villevalta.thingspeakclient.model.ChannelFeed;
@@ -22,12 +24,14 @@ import retrofit.client.Response;
 /**
  * Created by villevalta on 31.3.2015.
  */
-public class ChannelActivity extends ActionBarActivity {
+public class ChannelActivity extends ActionBarActivity implements View.OnClickListener {
 
     int mChannelId; // Channel id
     String mChannelReadKey;
 
     ChannelFeed mChannelFeed;
+
+    View mChannelView;
 
     // Channel info
     Toolbar mToolbar;
@@ -36,19 +40,25 @@ public class ChannelActivity extends ActionBarActivity {
 
     ChartView mChart1, mChart2, mChart3, mChart4, mChart5, mChart6, mChart7, mChart8;
 
+    // ErrorView
+    View mErrorView;
+    TextView mErrorMessage;
+    EditText mInputChannelId;
+    EditText mInputChannelReadKey;
+    BootstrapButton mRetryButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (!readParameters()) {
-            Toast.makeText(this, "Channel not found.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Invalid Channel ID.", Toast.LENGTH_SHORT).show();
             finish(); // Close if id was not given
         }
 
-        // Toolbar menu: auto refresh, favorite, add launcher shortcut
-
         setContentView(R.layout.activity_channel);
 
+        mChannelView = findViewById(R.id.ChannelView);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mDescriptionTextView = (TextView) findViewById(R.id.description);
         mUsernameTextView = (TextView) findViewById(R.id.username);
@@ -62,6 +72,14 @@ public class ChannelActivity extends ActionBarActivity {
         mChart7 = (ChartView) findViewById(R.id.chart7);
         mChart8 = (ChartView) findViewById(R.id.chart8);
 
+        mErrorView = findViewById(R.id.channelErrorView);
+        mErrorMessage = (TextView) findViewById(R.id.channelErrorMessage);
+        mInputChannelId = (EditText) findViewById(R.id.input_channelId);
+        mInputChannelReadKey = (EditText) findViewById(R.id.input_channelReadKey);
+        mRetryButton = (BootstrapButton) findViewById(R.id.channelRetryButton);
+
+        mRetryButton.setOnClickListener(this);
+
         setSupportActionBar(mToolbar);
 
         // TODO: Check if channel has settings saved, use those settings to get a single feed. if not, use default (last 100 data points)
@@ -72,17 +90,23 @@ public class ChannelActivity extends ActionBarActivity {
         return new Callback<ChannelFeed>() {
             @Override
             public void success(ChannelFeed channelFeed, Response response) {
-
                 mChannelFeed = channelFeed;
-
+                hideError();
                 updateChannelInfo();
                 updateCharts();
-
             }
 
             @Override
             public void failure(RetrofitError error) {
-                // Channel is private or does not exist (show inputs to change id and channel read key)
+                if(error.getResponse().getStatus() == 404){
+                    showError("Channel was not found.");
+                }else if(error.getResponse().getStatus() == 400){
+                    showError("This channel is private. Please provide a read key.");
+                }else{
+                    showError("Remote server returned with error code: " + error.getResponse().getStatus());
+                    Log.e("ERROR",error.getResponse().getStatus()+"");
+                }
+
             }
         };
     }
@@ -130,6 +154,22 @@ public class ChannelActivity extends ActionBarActivity {
         }
     }
 
+    private void showError(String statusMessage){
+        mErrorMessage.setText(statusMessage);
+        mInputChannelId.setText(""+mChannelId);
+        mInputChannelReadKey.setText(mChannelReadKey != null ? mChannelReadKey : "");
+        mRetryButton.setBootstrapButtonEnabled(true);
+        mChannelView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideError(){
+        mErrorView.setVisibility(View.GONE);
+        mChannelView.setVisibility(View.VISIBLE);
+    }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return super.onCreateOptionsMenu(menu);
@@ -142,4 +182,34 @@ public class ChannelActivity extends ActionBarActivity {
         return mChannelId > 0;
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == mRetryButton.getId()){
+
+            if(mInputChannelId.length() < 0){
+                mInputChannelId.setError("Please provide a valid ID");
+                return;
+            }
+
+            int newId = 0;
+            try {
+                newId = Integer.parseInt(mInputChannelId.getText().toString());
+            }catch (NumberFormatException ignored) {
+            }
+
+            if(newId <= 0){
+                mInputChannelId.setError("Please provide a valid ID");
+                return;
+            }
+
+            mChannelId = newId;
+
+            if (mInputChannelReadKey.length() > 0) {
+                mChannelReadKey = mInputChannelReadKey.getText().toString();
+            }
+
+            mRetryButton.setBootstrapButtonEnabled(false);
+            ApiClient.getInstance().getChannelFeed(mChannelId, mChannelReadKey, null, onInfoCallback());
+        }
+    }
 }
