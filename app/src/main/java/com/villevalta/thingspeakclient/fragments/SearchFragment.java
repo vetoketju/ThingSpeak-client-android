9e8746f0898ab7de58553ab4fbb73b5b0c1f24ca
Ville Valta <ville.valta@gmail.com>
2015-09-24 14:09:49 +0300

    removed annotation

--- app/src/main/java/com/villevalta/thingspeakclient/fragments/SearchFragment.java
+++ app/src/main/java/com/villevalta/thingspeakclient/fragments/SearchFragment.java
@@ -2,7 +2,6 @@ package com.villevalta.thingspeakclient.fragments;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -25,7 +24,6 @@ public class SearchFragment extends Fragment implements TabbedFragment {
 		return mViewPager;
 	}
 
-	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_search, container, false);
